763649ee9800ab9947d44e35372baa18d017afcc
Victoria Andrade <vandradev2@est.ups.edu.ec>
2025-07-01 08:48:45 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,48 @@
+import controller.Sets;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sets sets = new Sets();
+        System.out.println("Autor: Victoria Andrade");
+        runHashSet(sets);
+        System.out.println("\n");
+        runLinkedHashSet(sets);
+        System.out.println("\n");
+        runTreeSet(sets);
+        System.out.println("\n");
+        runComparationTreeSet(sets);
+    }
+
+    public static void runHashSet(Sets sets){
+        Set<String> ejemploHashSet = sets.construirHashSet();      
+        System.out.println("Elementos del HashSet: ");
+        for(String elemento : ejemploHashSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runLinkedHashSet(Sets sets){
+        Set<String> ejemploLinkedHashSet = sets.construirLinkedHashSet();
+        System.out.println("Elementos del Linked HashSet: ");
+        for(String elemento : ejemploLinkedHashSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runTreeSet(Sets sets){
+        Set<String> ejemploTreeSet = sets.construirTreeSet();
+        System.out.println("Elementos del Tree Set: ");
+        for(String elemento : ejemploTreeSet){
+            System.out.println(elemento);
+        }
+    }
+
+    public static void runComparationTreeSet(Sets sets){
+        Set<String> ejemploComparar = sets.construirTreeSetConComparador();
+        System.out.println("Elementos comparados: ");
+        for(String elemento : ejemploComparar){
+            System.out.println(elemento);
+        }
+    }
+}
---
+++ src/controller/Sets.java
@@ -0,0 +1,73 @@
+package controller;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+    public Sets(){
+
+    }
+    //vamos a hacer que el metodo retorne la variable
+    public Set<String> construirHashSet(){
+        Set<String> palabras = new HashSet<>(); //generico
+        palabras.add("Chocolate");
+        palabras.add("Computador");
+        palabras.add("Frutilla");
+        palabras.add("Chocolate");
+        palabras.add("Pera");
+        palabras.add("Computador");
+        return palabras;
+    }
+
+    public Set<String> construirLinkedHashSet(){
+        Set<String> linkedWords = new LinkedHashSet<>();
+        linkedWords.add("Chocolate");
+        linkedWords.add("Computador");
+        linkedWords.add("Frutilla");
+        linkedWords.add("Chocolate");
+        linkedWords.add("Pera");
+        linkedWords.add("Computador");
+        return linkedWords;
+    }
+
+    public Set<String> construirTreeSet(){
+        Set<String> treeWords = new TreeSet<>();
+        treeWords.add("Chocolate");
+        treeWords.add("Computador");
+        treeWords.add("Frutilla");
+        treeWords.add("Chocolate");
+        treeWords.add("Pera");
+        treeWords.add("Computador");
+        return treeWords;
+    }
+
+    public Set<String> construirTreeSetConComparador(){
+        //Crear un comparador
+        Comparator<String> comparadorLongitud = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2){
+                //Comparamos la longitud
+                int result = Integer.compare(s1.length(), s2.length());
+                //si tienen la misma longitud, comparamos alfabeticamente
+                if(result == 0){
+                    result = s1.compareToIgnoreCase(s2);
+                }
+                return result;
+            }
+        };
+
+        Set<String> treeWords = new TreeSet<>(comparadorLongitud);
+        treeWords.add("Laptop");
+        treeWords.add("Manzana");
+        treeWords.add("Pera");
+        treeWords.add("Celular");
+        treeWords.add("Laptop");
+        treeWords.add("Pera");
+        treeWords.add("Laptop");
+        treeWords.add("Celulas");
+        return treeWords;
+    }
+}
